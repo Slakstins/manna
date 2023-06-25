ed062b7b67a6f7081563183932c079e35f78a809
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2023-06-25 15:56:36 +0000

    Add dynamic determination of api server

--- manna-app/src/app/environment.ts
+++ manna-app/src/app/environment.ts
@@ -3,3 +3,7 @@ export const environment = {
   production: false,
   baseUrl: "http://localhost:3000/api/"
 };
+
+if (window.location.hostname !== 'localhost') {
+  environment.baseUrl = window.location.protocol + '//' + window.location.hostname + "/api/"
+}
